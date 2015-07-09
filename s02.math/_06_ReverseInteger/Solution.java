f2418a6e60d889dbc223ee7ac7ec23fdead3de37
Steven Cooks <stevenjoy1113@gmail.com>
2015-07-08 23:02:41 -0400

    2.06 (1) deal with stack overflow

---
+++ s02.math/_06_ReverseInteger/Solution.java
@@ -0,0 +1,33 @@
+/**
+ *************************************************************************
+ * Description: 
+ * 
+ * Reverse digits of an integer. 
+ * 
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321 
+ * 
+ * Return 0 if the result overflows and does not fit in a 32 bit signed integer
+ *
+ *************************************************************************
+ * @date : Jul 8, 2015
+ * 1. how to deal with Stack Overflow
+ */
+package _06_ReverseInteger;
+
+/** see test {@link _06_ReverseInteger.SolutionTest } */
+public class Solution {
+    public int reverse(int A) {
+        // use long to monitor Stack Overflow
+        long result = 0;
+        while (A != 0) {
+            result = result * 10 + (A % 10);
+            A = A / 10;
+        }
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            return 0;
+        } else {
+            return (int) result;
+        }
+    }
+}
